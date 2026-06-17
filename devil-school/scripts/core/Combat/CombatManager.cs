07133ab6e5481b113d241545b6de356cfa892cc6
Shao <1938957372@qq.com>
2026-06-18 02:06:04 +0800

    CombatManager框架

--- devil-school/scripts/core/Combat/CombatManager.cs
+++ devil-school/scripts/core/Combat/CombatManager.cs
@@ -1,14 +1,37 @@
 
 
+using System.Threading.Tasks;
+
 namespace EGame
 {
     /// <summary>
-    /// 管理战斗的逻辑，流程，数据全由CombatState管理
+    /// CombatManager仅仅管理战斗流程，数据由CombatRoom管理
     /// </summary>
     public class CombatManager
     {
         public static CombatManager Instance { get; } = new CombatManager();
 
         private CombatState _CombatState;
+
+        public void StartCombat(CombatState combat_state)
+        {
+            _CombatState = combat_state;
+            TaskHelper.RunSafely(StartCombatInternal());
+        }
+        private async Task StartCombatInternal()
+        {
+            await ExecutePlayerTurn();
+            await ExecuteEnemyTurn();
+        }
+
+        private async Task ExecutePlayerTurn()
+        {
+            await Task.CompletedTask;
+        }
+
+        private async Task ExecuteEnemyTurn()
+        {
+            await Task.CompletedTask;
+        }
     }
 }
