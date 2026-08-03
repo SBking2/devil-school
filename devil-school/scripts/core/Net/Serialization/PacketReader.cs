309adb9d7a22bb57df14c1ab6c2ad4bf2c6d2b11
Shao <1938957372@qq.com>
2026-08-04 03:03:59 +0800

    Serialization Coding

--- devil-school/scripts/core/Net/Serialization/PacketReader.cs
+++ devil-school/scripts/core/Net/Serialization/PacketReader.cs
@@ -1,8 +1,62 @@
 
+using Godot;
+using System;
+
 namespace EGame
 {
     public class PacketReader
     {
+        private byte[] _TempBuffer = new byte[16];  //当存入一个数据的时候，先把这个数据序列化存入这个buffer才方便统一进行字节拷贝
 
+        private byte[] _Buffer = new byte[512];  //用于存储接收到的完整数
+
+        private int _BitPosition = 0;  //当前读取到的bit位置
+        private int BytePosition => (int)((float)_BitPosition / 8f);
+
+        public void Reset()
+        {
+            _BitPosition = 0;
+        }
+
+        public void WriteBool(bool value)
+        {
+
+        }
+        public void WirteInt()
+        {
+
+        }
+
+        public void WriteUInt()
+        {
+
+        }
+        
+        public void WriteFloat()
+        {
+
+        }
+
+        public void WriteDouble()
+        {
+
+        }
+
+        private void EnsureBufferCapacity(int need_bit_length)
+        {
+            int current_length = _Buffer.Length;
+            int need_min_length = Mathf.CeilToInt((float)_BitPosition / 8f);
+            int need_length = _Buffer.Length;
+
+            while(need_length < need_min_length)
+                need_length *= 2;
+
+            if(need_length != current_length)
+            {
+                byte[] new_buffer = new byte[need_length];
+                Array.Copy(_Buffer, new_buffer, need_min_length);
+                _Buffer = new_buffer;
+            }
+        }
     }
 }
