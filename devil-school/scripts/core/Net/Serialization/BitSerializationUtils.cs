9a407a49b23cc3ba5cb8a0b26b8ca5467230c83a
Shao <1938957372@qq.com>
2026-08-05 02:08:45 +0800

    <feat>完善WriteBitBuffer

--- devil-school/scripts/core/Net/Serialization/BitSerializationUtils.cs
+++ devil-school/scripts/core/Net/Serialization/BitSerializationUtils.cs
@@ -7,20 +7,65 @@ namespace EGame
 {
     public static class BitSerializationUtils
     {
-        //bit级别的Copy
-        public static void WriteBytes(byte[] source_buffer, byte[] destination_buffer, int d_start_pos, int copy_length, int s_start_pos = 0)
+        //把低位(右边)当作起点,返回一个连续bits歌1，起点为start_bit的掩码
+        public static int GetBitMask(int bits, int start_bit)
         {
-            int des_length = destination_buffer.Length;
-            int source_length = source_buffer.Length;
+            if (bits > 8 || start_bit + bits > 8)
+                throw new InvalidOperationException();
 
-            int expected_des_length = Mathf.CeilToInt((float)d_start_pos + copy_length / 8f);
-            int expected_source_length = Mathf.CeilToInt((float)s_start_pos + copy_length / 8f);
+            return (1 << bits) - 1 << start_bit;
+        }
 
-            if (expected_des_length > des_length)
-                throw new InvalidOperationException($"Desitination doesn't have enough capacity! Length : {des_length} Expected : {expected_des_length}");
+        //取出buffer中从start_pos开始的bit_length位，返回一个byte（低位对齐）
+        public static byte GetBitInByte(byte[] buffer, int start_pos, int bit_length)
+        {
+            if(bit_length > 8 || start_pos + bit_length > buffer.Length * 8)
+                throw new InvalidOperationException();
 
-            if (expected_source_length > source_length)
-                throw new InvalidOperationException($"Source doesn't have enough capacity! Length : {source_length} Expected : {expected_source_length}");
+            int byte_index = start_pos / 8;
+            int low_length = start_pos % 8;
+
+            //情况1:开始位置正好是一个字节的结束，因此直接提取下一个字节的低bit_length位即可
+            if (low_length == 0)
+                return (byte)(buffer[byte_index] & GetBitMask(bit_length, 0));
+
+            //情况2:需要提取的bit在同一个字节内
+            int high_lengh = 8 - low_length;
+            if (high_lengh >= bit_length)
+                return (byte)((buffer[byte_index] >> low_length) & GetBitMask(bit_length, 0));
+
+            //情况3:跨字节读取,上一个字节拼在低位，下一个字节拼在高位
+            var byte1 = (byte)((buffer[byte_index] >> low_length) & GetBitMask(high_lengh, 0));
+            var byte2 = (byte)((buffer[byte_index + 1] << high_lengh) & GetBitMask(bit_length - high_lengh, high_lengh));
+            return (byte)((byte1 & GetBitMask(high_lengh, 0)) | (byte2 & GetBitMask(bit_length - high_lengh, high_lengh)));
+        }
+
+        //bit级别的Copy,注意字节内都说从低位开始copy
+        public static void WriteBytes(byte[] source_buffer, byte[] destination_buffer, int d_start_pos, int copy_length)
+        {
+            int write_length;
+            //一个一个字节的处理
+            for(int i = 0; i < copy_length; i += write_length)
+            {
+                int index = (d_start_pos + i) / 8;
+                int low_length = (d_start_pos + i) % 8;
+                write_length = Mathf.Min(copy_length - i, 8 - low_length);
+
+                //获取到了当前字节可写write_length，则开始准备数据
+                var data = GetBitInByte(source_buffer, i, write_length);
+
+                //获取原字节中干净的末尾
+                var clear_des_byte = (byte)(destination_buffer[index] & GetBitMask(low_length, 0));
+
+                //把获取到的可写bit插入到末尾上
+                destination_buffer[index] = (byte)(clear_des_byte | (data << low_length));
+            }
+        }
+       
+        //同Write，就算把source s_start_pos开始的长度为read_length的数据，写到destination_buffer里
+        public static void ReadBits(byte[] source_buffer, byte[] destination_buffer, int s_start_pos, int read_length)
+        {
+
         }
     }
 }
